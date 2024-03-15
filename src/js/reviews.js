72b7f99ea8286e457af608b04f975338c62681d1
Serj21 <zundo24226@gmail.com>
2024-03-15 12:10:09 +0200

    123

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,11 +1,11 @@
 import Axios from 'axios';
 import Swiper from 'swiper';
-import 'swiper/css';
+import 'swiper/swiper-bundle.css';
 
-// import { Navigation, Pagination } from 'swiper/modules';
+const listEl = document.querySelector('.reviews-list');
+const prevButton = document.querySelector('.swiper-button-prev');
+const nextButton = document.querySelector('.swiper-button-next');
 
-const swiperContainer = document.querySelector('.mySwiper');
-const swiperWrapper = document.querySelector('.swiper-wrapper');
 const axios = Axios.create({
   baseURL: 'https://portfolio-js.b.goit.study/api/reviews',
 });
@@ -15,46 +15,67 @@ async function getReviewsData() {
     const res = await axios.get();
     return res.data;
   } catch (error) {
-    console.log(error);
+    console.error('Error fetching reviews:', error);
+    return [];
   }
 }
 
 async function renderReviewsCard() {
-  const reviewsData = await getReviewsData();
-  let markup = '';
-  reviewsData.forEach(data => {
-    markup += `<div class="swiper-slide">
-    <div class="author">
-        <img src=${data.avatar_url} alt="Avatar" class="avatar" width="48" height="48">
-        <h3 class="name">${data.author}</h3>
-    </div>
-    <p class="review-text">
-        ${data.review}
-    </p>
-</div>`;
-  });
-  swiperWrapper.insertAdjacentHTML('beforeend', markup);
+  try {
+    const reviewsData = await getReviewsData();
+    if (reviewsData.length === 0) {
+      listEl.innerHTML = '<li>Not Found</li>';
+      return;
+    }
+    let markup = '';
+    reviewsData.forEach(data => {
+      markup += `<li><div class="swiper-slide">
+          <div class="author">
+            <img src="${data.avatar_url}" alt="Avatar" class="avatar" width="48" height="48">
+            <h3 class="name">${data.author}</h3>
+          </div>
+          <p class="review-text">${data.review}</p>
+        </div></li>`;
+    });
+    listEl.innerHTML = markup;
+  } catch (error) {
+    console.error('Error rendering reviews:', error);
+  }
 }
-renderReviewsCard();
 
-const swiper = new Swiper('.mySwiper', {
-  slidesPerView: 1,
-  spaceBetween: 16,
-  breakpoints: {
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 16,
+async function initSwiper() {
+  await renderReviewsCard();
+  const swiper = new Swiper('.mySwiper', {
+    slidesPerView: 1,
+    spaceBetween: 16,
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 16,
+      },
+      1440: {
+        slidesPerView: 3,
+      },
     },
-    1440: {
-      slidesPerView: 3,
+    navigation: {
+      prevEl: prevButton,
+      nextEl: nextButton,
     },
-  },
-  //   Navigation: {
-  //     nextEL: 'swiper-button-next',
-  //     preEl: 'swiper-button-prev',
-  //   },
-  //   pagination: {
-  //     el: '.swiper-pagination',
-  //     clickable: true,
-  //   },
-});
+    on: {
+      slideChange: function () {
+        if (swiper.isBeginning) {
+          prevButton.classList.add('swiper-button-disabled');
+        } else {
+          prevButton.classList.remove('swiper-button-disabled');
+        }
+        if (swiper.isEnd) {
+          nextButton.classList.add('swiper-button-disabled');
+        } else {
+          nextButton.classList.remove('swiper-button-disabled');
+        }
+      },
+    },
+  });
+}
+
+initSwiper();
